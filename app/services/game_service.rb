afe309ccf4730e7f9ccccd6948d1386e1b0e1272
CeeBeeUK <colinbruce@gmail.com>
2014-08-04 22:29:46 +0100

    minor updates and working winner creation

--- app/services/game_service.rb
+++ app/services/game_service.rb
@@ -8,13 +8,16 @@ class GameService
     [7,6,5,4,3,2,1, 2, 3, 4, 5,6,7].each do |card|
       @new_hand = Hand.new(no_of_cards: card, suit: @this_suit )
       @game.hands << @new_hand
-      if trump_type==2
+      if @game.trump_type_id==2
         @this_suit = Suit.find(1)
       else
         x = (x<=2 ? x+1 : 0)
         @this_suit = Suit.find(@trump_sequence[x])
       end
     end
+    # &#x1F0A1; = Ace of spades
+    # &spades;
+    # http://en.wikipedia.org/wiki/Playing_Cards_(Unicode_block)
     @game
   end
 
